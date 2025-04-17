9c44f913ebdf1ff68f7880208cb5159f8fb7d4b3
JeremyRoalef <roalef944@morrisville.edu>
2025-04-17 19:07:13 -0400

    Integrated NavMesh Robot control via Joystick

--- VRCourse/Assets/Scripts/Interactables/NavMeshJoystick.cs
+++ VRCourse/Assets/Scripts/Interactables/NavMeshJoystick.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,24 +6,49 @@ using UnityEngine;
 public class NavMeshJoystick : SimpleHingeInteractable
 {
     [SerializeField]
+    NavMeshRobot robot;
+
+    [SerializeField]
     Transform trackingObject;
 
     [SerializeField]
     Transform trackedObject;
 
+    [SerializeField]
+    Transform rotationParentObj;
+
     protected override void ResetHinge()
     {
-
+        if (robot != null)
+        {
+            robot.StopAgent();
+        }
     }
 
     protected override void Update()
     {
         base.Update();
 
-        trackingObject.position = new Vector3(
-            trackedObject.position.x, 
-            trackingObject.position.y, 
-            trackedObject.position.z
-            );
+        if (isSelected)
+        {
+            MoveRobot();
+        }
+    }
+
+    private void MoveRobot()
+    {
+        if (robot != null)
+        {
+            trackingObject.position = new Vector3(
+                trackedObject.position.x,
+                trackingObject.position.y,
+                trackedObject.position.z
+                );
+
+            rotationParentObj.rotation = Quaternion.identity;
+
+
+            robot.MoveAgent(trackingObject.localPosition);
+        }
     }
 }
